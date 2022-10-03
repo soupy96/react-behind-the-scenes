8c320e7f1120e300f3053ce8b59065b799f12ce3
Michael Campbell <michael.campbell14@gmail.com>
2022-10-03 18:11:43 -0400

    using and learning about React.memo

--- src/App.js
+++ src/App.js
@@ -14,10 +14,13 @@ function App() {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
   };
 
+  // if the app component is re-evaluted all the child components will be re-evaluated as well
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
+      {/* DemoOutput is not re-executed because false is always false. false === false */}
+      <DemoOutput show={false} />
+      {/* Button is re-executed with memo because a function is not the same as its own function even though its the same function both times. */}
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import MyParagraph from './MyParagraph';
 
 const DemoOutput = (props) => {
-  return <p>{props.show ? 'This is new!' : ''}</p>;
+  console.log('DEMO RUNNING');
+
+  return <MyParagraph>{props.show ? 'This is new!' : ''}</MyParagraph>;
 };
 
-export default DemoOutput;
+// for functional components, not class based
+// React.memo optomize functional components
+// memo tells reacts for this components that it gets as a argument, react should look at the props that this component gets and compare the new value it just got to the previous value that it previously had
+// only if the value has changed the components will be re-executed/re-evaluted
+// we dont want to use this for everything tho cause it needs to store the values for the previous and changed values for props. this has its own performance issues.
+export default React.memo(DemoOutput);
